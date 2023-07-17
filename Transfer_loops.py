19f58e299fd6bc0ad5f77f1ebd073e5beb989ac0
Marielena Velasco Enriquez <menriquez@iap.phy.tu-dresden.de>
2023-07-17 19:06:59 +0200

    plotting new data

--- Transfer_loops.py
+++ Transfer_loops.py
@@ -11,8 +11,9 @@ plt.rcParams.update({'font.size':20})
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230621_pg3t_f6tcnnq10_photopattternSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230629_doped_pg3t_5mgmlF6TCNNQ_photopattternSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230706_pg3t_photopattternSSE_remainings13.05'
-dir_path = 'Data\\3. Transfer curves\\1. IV\\230711_doped_pg3t_15mgmlF6TCNNQ_printedSSE'
+#dir_path = 'Data\\3. Transfer curves\\1. IV\\230711_doped_pg3t_15mgmlF6TCNNQ_printedSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230711_doped_pg3t_5mgmlF6TCNNQ_printedSSE'
+dir_path = 'Data\\3. Transfer curves\\1. IV\\230712_doped_pg3t_15mgmlF6TCNNQ_AgNP_printedSSE'
 
 ##Bioprobe
 #dir_path = 'Data\\3. Transfer curves\\2. Bioprobe\\230616_pg3t_photopattternSSE'
@@ -35,16 +36,17 @@ column_vgs=6
 column_loop=column_vgs-1
 
 
-## Doping
-#d1 = 26.2/40
-#d2 = 27.2/40
-## Undoping
-d1 = 33/40
-d2 = 34/40
+## Forward
+d1 = 25.8/40
+d2 = 26.5/40
+
+## Backward
+#d1 = 34/40
+#d2 = 34.5/40
 
 for i in range(len(transfer)):
     ## Get gradient plots, just one vds but multiple loops
-    plot_transfer_curves_one_vds(T[i], transfer[i], column_ids, column_vgs, column_loop)
+    #plot_transfer_curves_one_vds(T[i], transfer[i], column_ids, column_vgs, column_loop)
     ## Calculate Vth for multiple loops
     calculate_vth_one_vds(T[i], transfer[i], d1, d2, column_ids, column_vgs, column_loop)
 
