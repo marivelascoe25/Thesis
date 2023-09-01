2d4840b724abbeeb98bf2ba52cc3c9b5c712a301
Marielena Velasco Enriquez <menriquez@iap.phy.tu-dresden.de>
2023-09-01 17:38:31 +0200

    new images and changes in doped section

--- Transfer_loops.py
+++ Transfer_loops.py
@@ -7,7 +7,7 @@ dir_path = 'Data\\3. Transfer curves\\1. IV\\230613_pg3t_photopattternSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230614_pg3t_photopattternSSE_before'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230614_pg3t_photopattternSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230615_pg3t_photopattternSSE_from13again'
-#dir_path = 'Data\\3. Transfer curves\\1. IV\\230621_pg3t_f6tcnnq10_photopattternSSE'
+#dir_path = 'Data\\3. Transfer curves\\1. IV\\230621_pg3t_f6tcnnq10_photopattternSSE' ## dope10 that i fuck with adhesion promoter
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230629_doped_pg3t_5mgmlF6TCNNQ_photopattternSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230706_pg3t_photopattternSSE_remainings13.05'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230711_doped_pg3t_15mgmlF6TCNNQ_printedSSE'
@@ -27,9 +27,11 @@ dir_path = 'Data\\3. Transfer curves\\1. IV\\230613_pg3t_photopattternSSE'
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230720_undoped_pg3t_inkSSE'
 #doped U7
 #dir_path = 'Data\\3. Transfer curves\\1. IV\\230711_doped_pg3t_5mgmlF6TCNNQ_printedSSE'
+#dir_path = 'Data\\3. Transfer curves\\1. IV\\230629_doped_pg3t_10mgmlF6TCNNQ_printedSSE' #review with Anton if include D2
 
 ## Store files
 transfer, out = read_directory_transfer(dir_path)
+
 
 ## Get plot titles
 T = plot_titles(transfer)
@@ -47,12 +49,12 @@ column_loop=column_vgs-1
 
 
 ## Forward
-#d1 = 25.8/40
-#d2 = 26.5/40
+#d1 = 25/40
+#d2 = 26/40
    
 ## Backward
-d1 = 34/40
-d2 = 34.5/40
+d1 = 34.2/40
+d2 = 35.2/40
 
 number = 3
 #gm_values = [0.0 for i in range (len(transfer))] 
@@ -62,7 +64,7 @@ for i in range(len(transfer)):
     ## Get gradient plots, just one vds but multiple loops
     X,Y,Z, gm_values = plot_transfer_curves_one_vds(T[i], transfer[i], column_ids, column_vgs, column_loop, number, trans=True)
     ## Calculate Vth for multiple loops
-    #calculate_vth_one_vds(T[i], transfer[i], d1, d2, column_ids, column_vgs, column_loop)
+    calculate_vth_one_vds(T[i], transfer[i], d1, d2, column_ids, column_vgs, column_loop)
     print(T[i])
     print (max(gm_values))
 #except:
